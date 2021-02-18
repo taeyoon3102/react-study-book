119e219b0ad71bdb45131e2f701bfabcea3aad08
okdom <forlegions@gmail.com>
2021-02-18 17:21:53 +0900

    commit

---
+++ src/example/R003_Component.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+// class R003Component extends Component{
+//     render(){
+//         return (
+//             <h2>[ THIS IS IMPORTED COMPONENT ]</h2>
+//         )
+//     }
+// }
+
+// export default R003Component;
+
+export default function R003_Component(){
+
+    return(
+        <h2>[ THIS IS IMPORTED COMPONENT ]</h2>
+    )
+        
+}
